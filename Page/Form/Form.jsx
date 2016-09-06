5dbc6e44cd716dc386ce9fef4e85c8be12fea3aa
woolseyyy <woolsey@zju.edu.cn>
2016-09-06 23:10:11 +0800

    文件上传

--- Page/Form/Form.jsx
+++ Page/Form/Form.jsx
@@ -66,6 +66,16 @@ var Form=React.createClass({
                     type:'multi-choose',
                     title:'面试时间',
                     chosen:[]
+                },
+                {
+                    type:'file',
+                    title:'个人简历',
+                    url:''
+                },
+                {
+                    type:'file',
+                    title:'相关作品',
+                    url:''
                 }
             ],
             remark:''
@@ -82,6 +92,128 @@ var Form=React.createClass({
                 midHeight:midHeight-135
             })
         }.bind(this))
+
+        //七牛文件上传 照片
+        var uploader = Qiniu.uploader({
+            runtimes: 'html5,flash,html4',
+            browse_button: 'photo',
+            uptoken_url: 'http://182.254.157.172:3000/uptoken',
+            domain: 'http://ocsdd1fl7.bkt.clouddn.com/',   //bucket 域名，下载资源时用到，**必需**
+            get_new_uptoken: false,  //设置上传文件的时候是否每次都重新获取新的token
+            max_file_size: '10mb',           //最大文件体积限制
+            flash_swf_url: 'js/plupload/Moxie.swf',  //引入flash,相对路径
+            max_retries: 3,                   //上传失败最大重试次数
+            dragdrop: true,                   //开启可拖曳上传
+            chunk_size: '4mb',                //分块上传时，每片的体积
+            auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传
+            unique_names: true,              //自动生成key
+            init: {
+                'BeforeUpload': function(up, file) {
+                    $('#loading').fadeIn();
+                    $(document).on('click', '#cancel', function(){
+                        this.stop();
+                        $('#loading').fadeOut();
+                        $(document).off('click', '#cancel');
+                    }.bind(this));
+
+                },
+                'UploadProgress': function(up, file) {
+                    $('#loadingPercentage').text('已上传 '+file.percent+'%');
+                    $('#loadingSpeed').text(file.speed/1000+'kb/s');
+                },
+                'FileUploaded': function(up, file, info) {
+                    var domain = up.getOption('domain');
+                    var res = $.parseJSON(info);
+                    var sourceLink = domain + res.key;
+                    this.setState({'img':sourceLink});
+
+                    //上传提示消失 相关事件解绑
+                    $('#loading').fadeOut();
+                    $(document).off('click', '#cancel');
+                }.bind(this),
+                'Error': function(up, err, errTip) {
+                    //上传提示消失 相关事件解绑
+                    $('#loading').fadeOut();
+                    $(document).off('click', '#cancel');
+
+                    //上传出错时,处理相关的事情
+                    if(err.code== -600){//文件大小过大
+                        var limit = up.getOption('max_file_size');
+                        alert('上传文件大小不得超过'+limit);
+                    }
+                    else {
+                        alert('上传出错，请重试\n'+errTip);
+                    }
+                }.bind(this)
+            }
+        });
+
+        //七牛文件上传 其他组件
+        var others = this.state.others;
+        for(var i=0; i<others.length; i++){
+            var other = others[i];
+            if(other.type=='file'){
+                Qiniu.uploader({
+                    index: i,
+                    runtimes: 'html5,flash,html4',
+                    browse_button: 'file'+i,
+                    uptoken_url: 'http://182.254.157.172:3000/uptoken',
+                    domain: 'http://ocsdd1fl7.bkt.clouddn.com/',   //bucket 域名，下载资源时用到，**必需**
+                    get_new_uptoken: false,  //设置上传文件的时候是否每次都重新获取新的token
+                    max_file_size: '100mb',           //最大文件体积限制
+                    flash_swf_url: 'js/plupload/Moxie.swf',  //引入flash,相对路径
+                    max_retries: 3,                   //上传失败最大重试次数
+                    dragdrop: true,                   //开启可拖曳上传
+                    chunk_size: '4mb',                //分块上传时，每片的体积
+                    auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传
+                    unique_names: true,              //自动生成key
+                    multi_selection: false,         //一次只允许一个文件上传
+                    init: {
+                        'BeforeUpload': function(up, file) {
+                            $('#loading').fadeIn();
+                            $(document).on('click', '#cancel', function(){
+                                this.stop();
+                                $('#loading').fadeOut();
+                                $(document).off('click', '#cancel');
+                            }.bind(this));
+                        },
+                        'UploadProgress': function(up, file) {
+                            $('#loadingPercentage').text('已上传 '+file.percent+'%');
+                            $('#loadingSpeed').text(file.speed/1000+'kb/s');
+                        },
+                        'FileUploaded': function(up, file, info) {
+                            var domain = up.getOption('domain');
+                            var i = up.getOption('index');
+                            var res = $.parseJSON(info);
+                            others[i].url = domain + res.key;
+                            this.setState({others:others});
+                            var target = this.refs['file'+i];
+                            $(target).text(file.name);
+
+                            //上传提示消失 相关事件解绑
+                            $('#loading').fadeOut();
+                            $(document).off('click', '#cancel');
+                        }.bind(this),
+                        'Error': function(up, err, errTip) {
+
+                            //上传提示消失 相关事件解绑
+                            $('#loading').fadeOut();
+                            $(document).off('click', '#cancel');
+
+                            //上传出错时,处理相关的事情
+                            if(err.code== -600){//文件大小过大
+                                var limit = up.getOption('max_file_size');
+                                alert('上传文件大小不得超过'+limit);
+                            }
+                            else {
+                                alert('上传出错，请重试\n'+errTip);
+                            }
+
+                        }.bind(this)
+                    }
+                });
+            }
+        }
     },
     submit: function(){
         $.ajax({
@@ -226,6 +358,23 @@ var Form=React.createClass({
                     titleTemplate="报名表 | %s"
                 />
                 <div className={style.frame}>
+                    <div id="loading">
+                        <div className="cssload-container">
+                            <div className="cssload-shaft1"></div>
+                            <div className="cssload-shaft2"></div>
+                            <div className="cssload-shaft3"></div>
+                            <div className="cssload-shaft4"></div>
+                            <div className="cssload-shaft5"></div>
+                            <div className="cssload-shaft6"></div>
+                            <div className="cssload-shaft7"></div>
+                            <div className="cssload-shaft8"></div>
+                            <div className="cssload-shaft9"></div>
+                            <div className="cssload-shaft10"></div>
+                        </div>
+                        <div className="loading-text" id="loadingPercentage"></div>
+                        <div className="loading-text" id="loadingSpeed"></div>
+                        <div className="loading-button" id="cancel">取消上传</div>
+                    </div>
                     <div className={style.head}>INA秋季纳新报名表--{this.state.wish.chosen}</div>
                     <div className={style.content}>
                         <div className={style.title}>
@@ -308,6 +457,13 @@ var Form=React.createClass({
                                 </tr>
                             </tbody>
                         </table>
+                        <div className={style.photoUp}>
+                            <div className={style.img}>
+                                <img src={(this.state.baseinfo.img)?this.state.baseinfo.img:"/Page/Form/static/photo.png"} alt="照片"/>
+                            </div>
+                            <div className={style.aUp} id="photo">上传照片</div>
+                            <div className={style.text}>照片大小50kb以内，支持jpg、png、bmp等格式</div>
+                        </div>
                     </div>
                     <div className={style.content}>
                         <div className={style.title}>
@@ -425,6 +581,34 @@ var Form=React.createClass({
                                 <td>
                                     <input type="checkbox" name="time" className={style.checkbox}
                                            onClick={function(event){var checked = (event.target.checked)?1:0; this.dataPass('周日晚上', null, 4, 3, checked)}.bind(this)}/>周日晚上
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className={style.content}>
+                        <div className={style.title}>
+                            <img className={style.titleImg} src="/Page/Form/static/files.png" alt="面试时间"/>
+                            <big className={style.titleText}>附件上传</big>
+                        </div>
+                        <table className={style.files}>
+                            <tbody>
+                            <tr>
+                                <td className={style.label}>
+                                    个人简历
+                                </td>
+                                <td>
+                                    <div ref="file4" className={style.filename}>您还未上传任何文件</div>
+                                    <div id="file4" className={style.fileButton}>上传</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className={style.label}>
+                                    相关作品
+                                </td>
+                                <td>
+                                    <div ref="file5" className={style.filename}>您还未上传任何文件</div>
+                                    <div id="file5"  className={style.fileButton}>上传</div>
                                 </td>
                             </tr>
                             </tbody>
